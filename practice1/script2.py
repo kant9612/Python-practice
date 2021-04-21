1e429eff579e6e9cf8ed9f3e3ceba16683351d8d
kant9612 <kanstephentakayama@gmail.com>
2021-04-21 10:14:03 +0000

    progate1-6~10

--- practice1/script2.py
+++ practice1/script2.py
@@ -1,3 +1,4 @@
+# 1-6
 apple_price = 200
 apple_count = 8
 
@@ -6,3 +7,42 @@ total_price = apple_price * apple_count
 
 # total_priceの値を出力してください
 print(total_price)
+
+# 1-7
+money = 2000
+print(money)
+
+# 変数moneyに5000を足して、変数moneyを上書きしてください
+money = money + 5000
+
+# 変数moneyの値を出力してください
+print(money)
+
+# 1-8
+# my_nameという変数に「にんじゃわんこ」という文字列を代入してください
+my_name = "にんじゃわんこ"
+
+# my_nameを用いて、「私はにんじゃわんこです」となるように変数と文字列を連結して出力してください
+print("私は" + my_name + "です")
+
+# 1-9
+age = 24
+# ageを用いて「私は24歳です」と出力してください
+print("私は" + str(age) + "歳です")
+
+count = '5'
+# countに1を足した値を出力してください
+print(int(count) + 1)
+
+# 1-10
+x = 7 * 10
+y = 5 * 6
+
+# xが70と等しい場合に「xは70です」と出力してください
+if x == 70:
+    print("xは70です")
+
+
+# yが40と等しくない場合に「yは40ではありません」と出力してください
+if y != 40:
+    print("yは40ではありません")
